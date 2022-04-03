d91a7d0e9852c00261ef3f86c3c0fe7cf2cc2b88
unknown <eteg@raiden.sede.eteg.net>
2022-04-03 15:25:48 -0300

    adicionando linter no projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import SearchForm from './components/SearchForm';
-import { Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchForm from "./components/SearchForm";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
       <Container>
-        <SearchForm></SearchForm>
+        <SearchForm />
       </Container>
     </div>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import {Form, Row, Col, Button, FloatingLabel} from 'react-bootstrap';
+import { Form, Row, Col, Button, FloatingLabel } from "react-bootstrap";
 
 function SearchForm() {
   return (
     <Form>
-        <Row>
-            <Col>
-                <FloatingLabel controlId="cepInput" label="CEP que será pesquisado:">
-                    <Form.Control type="text" placeholder="CEP que será pesquisado:" />
-                </FloatingLabel>
-            </Col>
-            <Col>
-                <Button variant="primary">Pesquisar</Button>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <Form.Label>Última pesquisa:</Form.Label>
-                <Form.Control disabled as="textarea" rows={3} />
-            </Col>
-        </Row>
+      <Row>
+        <Col>
+          <FloatingLabel controlId="cepInput" label="CEP que será pesquisado:">
+            <Form.Control type="text" placeholder="CEP que será pesquisado:" />
+          </FloatingLabel>
+        </Col>
+        <Col>
+          <Button variant="primary">Pesquisar</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Label>Última pesquisa:</Form.Label>
+          <Form.Control disabled as="textarea" rows={3} />
+        </Col>
+      </Row>
     </Form>
   );
 }
